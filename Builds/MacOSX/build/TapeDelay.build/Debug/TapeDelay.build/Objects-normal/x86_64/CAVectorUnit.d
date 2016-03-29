8cf8595213195b0fee0281f518c944d4ae2f8f38
google.username <alibarker89@gmail.com>
2016-03-29 16:57:19 +0100

    Some files removed

--- Builds/MacOSX/build/TapeDelay.build/Debug/TapeDelay.build/Objects-normal/x86_64/CAVectorUnit.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/alibarker89/SDKs/JUCE/modules/juce_audio_plugin_client/AU/CoreAudioUtilityClasses/CAVectorUnit.cpp \
-  /Users/alibarker89/SDKs/JUCE/modules/juce_audio_plugin_client/AU/CoreAudioUtilityClasses/CAVectorUnit.h \
-  /Users/alibarker89/SDKs/JUCE/modules/juce_audio_plugin_client/AU/CoreAudioUtilityClasses/CAVectorUnitTypes.h
--- Builds/MacOSX/build/TapeDelay.build/Debug/TapeDelay.build/Objects-normal/x86_64/CarbonEventHandler.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/alibarker89/SDKs/JUCE/modules/juce_audio_plugin_client/AU/CoreAudioUtilityClasses/CarbonEventHandler.cpp \
-  /Users/alibarker89/SDKs/JUCE/modules/juce_audio_plugin_client/AU/CoreAudioUtilityClasses/../../../juce_core/native/juce_mac_ClangBugWorkaround.h \
-  /Users/alibarker89/SDKs/JUCE/modules/juce_audio_plugin_client/AU/CoreAudioUtilityClasses/CarbonEventHandler.h
